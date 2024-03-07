0fe2d5b28bd7698255bf5e0e542080f469156024
JNiii <ineojay@gmail.com>
2024-03-07 20:53:17 +0800

    Initial commit of all files for both Frontend and Backend

---
+++ backend/GetAzureResume/__init__.py
@@ -0,0 +1,24 @@
+import logging
+import azure.functions as func
+import json
+import os
+
+from azure.cosmos import CosmosClient
+
+URL = os.environ["COSMOSDB_URI"]
+KEY = os.environ["COSMOSDB_KEY"]
+client = CosmosClient(URL, credential=KEY)
+database = client.get_database_client("AzureResume")
+container = database.get_container_client("Counter")
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    item = container.read_item(item="1", partition_key="1")
+    item['count'] += 1
+    new_item = container.replace_item(item=item, body=item)
+    return func.HttpResponse(
+        json.dumps(new_item),
+        mimetype='application/json',
+        status_code=200
+        )
---
+++ frontend/counter.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const localfunctionApi = 'http://localhost:7071/api/GetAzureResume';
+const azurefunctionApi = 'https://fa-getazureresume.azurewebsites.net/api/GetAzureResume';
+
+const getVisitCount = () => {
+    let count = 1;
+    fetch(azurefunctionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
